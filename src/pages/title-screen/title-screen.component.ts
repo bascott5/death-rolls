8134139776ff73197c949bf677a5f6bf0a2997dd
Bennett <bascott1002@gmail.com>
2025-03-31 23:46:57 -0400

    Adjusted title page

--- src/pages/title-screen/title-screen.component.ts
+++ src/pages/title-screen/title-screen.component.ts
@@ -1,23 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { ModalComponent } from '../../components/modal/modal.component';
 
 @Component({
   selector: 'title-screen',
   standalone: true,
-  imports: [RouterLink, ModalComponent],
+  imports: [RouterLink],
   templateUrl: './title-screen.component.html',
   styleUrl: './title-screen.component.css'
 })
 export class TitleScreenComponent {
-  isInstructionsOpen: boolean = false;
   constructor() {}
-
-  openInstructions() {
-    this.isInstructionsOpen = true;
-  }
-
-  closeInstructions() {
-    this.isInstructionsOpen = false;
-  }
 }
